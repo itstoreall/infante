d51ea7b144e6b7df006d7854cea9b5e44ca54330
Serhii Stanislav <serhiistanislav@gmail.com>
2020-09-28 01:56:01 +0300

    adds initial form

---
+++ form/form.js
@@ -0,0 +1,32 @@
+let selector = document.querySelectorAll("input[type=tel]");
+let ins = new Inputmask("+38 (999) 999 99 99");
+ins.mask(selector);
+
+let validateForms = function (selector, rules) {
+  new window.JustValidate(selector, {
+    rules: rules,
+    submitHandler: function (form) {
+      let formData = new FormData(form);
+
+      let xhr = new XMLHttpRequest();
+
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            console.log("sent");
+          }
+        }
+      };
+      xhr.open("POST", "mail.php", true);
+      xhr.send(formData);
+
+      form.reset();
+      // window.location = "/index.html";
+    },
+  });
+};
+
+validateForms(".slider-form", {
+  name: { required: true },
+  phone: { required: true },
+});
